943dc43a49e9a48b36157483ce2b67b0457f9435
Nilesh-Bhor <nilesh.bhor@cennest.com>
2019-01-15 02:46:44 -0800

    console app changed to net framwork

---
+++ BasicConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using GoDogServer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GoDogProcess goDogProcess = GoDogProcess.GetGoDogProcess("rtsp://admin:Foxhound1!@192.168.1.64/Streaming/Channels/1", "rtmp://104.248.182.51/live/2");
+            goDogProcess.StartConversion();
+            Console.Read();
+        }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,8 +1,4 @@
-﻿using GoDogMqttClient;
-using GoDogServer;
-using System;
-
-namespace ConsoleApp1
+﻿namespace ConsoleApp1
 {
     class Program
     {
@@ -11,7 +7,7 @@ namespace ConsoleApp1
            GoDogProcess goDogProcess = GoDogProcess.GetGoDogProcess("rtsp://admin:Foxhound1!@192.168.1.64/Streaming/Channels/1", "rtmp://104.248.182.51/live/2");
            goDogProcess.StartConversion();
 
-            Console.Read();
+            Console.ReadLine();
         }
     }
 }
